9a65a52426364ada59622af65dab80414c91efaa
Guillermo Duque <gui.duquesanchez@gmail.com>
2021-09-20 21:48:56 -0300

    schema and data files have been added. In addition, a property has been added to avoid conflicts in the autogeneration of tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,8 @@
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS accounts;
 
-CREATE TABLE customer (
-    customer_id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar (100) NOT NULL,
-    email varchar (100) NOT NULL,
-    mobile_numer varchar(20) NOT NULL,
-    create_dt date  DEFAULT NULL
 
-);
+
+INSERT INTO customer (name,email,mobile_numer,create_dt)
+VALUES ('emma','emma@gmail.com','789466', CURDATE());
+
+INSERT INTO accounts (customer_id,account_number,account_type, branch_address ,create_dt)
+VALUES (1, 186734,'Savings','123456 second street', CURDATE());
